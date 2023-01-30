a28525f42e33da3ec79b7cf575b7369e5014fb88
Karlis <karlisstekels.dev@gmail.com>
2023-01-30 10:47:42 +0200

    Initial Commit

---
+++ ExploringCharts/ExploringChartsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExploringChartsApp.swift
+//  ExploringCharts
+//
+//  Created by karlis.stekels on 30/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct ExploringChartsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
